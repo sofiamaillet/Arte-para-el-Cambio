97ac6e8a7f7607f5b88ecaa2613079ae516a4cef
sofiamaillet <56874389+sofiamaillet@users.noreply.github.com>
2023-08-04 22:04:40 +0200

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,17 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject= $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@yourwebsite.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name."
+
+
+
+
+
+?>
